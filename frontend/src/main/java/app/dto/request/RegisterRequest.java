4860a9cc8259ac82e76b3773e82e94152e043d49
unknown <symrfan@gmail.com>
2026-07-15 20:41:33 +0330

    RegisterRequest.java was added

---
+++ frontend/src/main/java/app/dto/request/RegisterRequest.java
@@ -0,0 +1,29 @@
+package app.dto.request;
+
+import java.io.Serializable;
+
+public class RegisterRequest implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String username;
+    private String password;
+    private String fullName;
+    private String phoneNumber;
+    private String email;
+
+    // Getters and Setters
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public String getFullName() { return fullName; }
+    public void setFullName(String fullName) { this.fullName = fullName; }
+
+    public String getPhoneNumber() { return phoneNumber; }
+    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+}
